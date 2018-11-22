9d589d233e5bb947fe40d4c285e7d30be02ca887
setakarim <alkariim.tridek@gmail.com>
2018-11-22 09:59:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,181 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" href="img/favicon.png" type="image/png">
+        <title>Puri Kalimaya</title>
+		<!-- Bootstrap CSS -->
+		<style type="text/css">
+			#satu, #dua, #tiga, #tigabelas, #tombolmaterial, #tombolprojet, #tomboltenagakerja, #tombolberanda, #duabelas
+			, #tombolpartner, #tomboltransaksi, #tombolfinansial, #tombolfixasset
+			{
+				position: absolute;
+				height: 479px;
+			}
+			#satu
+			{
+				width: 237px;
+				left: 27px;
+				top: 27px;
+				z-index: 3;
+			
+			}
+			
+		</style>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/mine.css">
+        <link rel="stylesheet" href="vendors/linericon/style.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+        <link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
+        <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+        <link rel="stylesheet" href="vendors/animate-css/animate.css">
+        <link rel="stylesheet" href="vendors/popup/magnific-popup.css">
+        <!-- main css -->
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+    </head>
+    <body data-spy="scroll" data-target="#mainNav" data-offset="70">
+        
+        <!--================Header Menu Area =================-->
+        <header class="header_area">
+            <div class="main_menu" id="mainNav">
+            	<nav class="navbar navbar-expand-lg navbar-light">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<a class="navbar-brand logo_h" href="index.html"><img width="150px" height="100px" src="img/logo_purikalimaya_final.jpg" alt=""></a>
+						<!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> -->
+							<!-- <span class="icon-bar"></span> -->
+							<!-- <span class="icon-bar"></span> -->
+							<!-- <span class="icon-bar"></span> -->
+						<!-- </button> -->
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<!-- <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+							<ul class="nav navbar-nav menu_nav ml-auto">
+								<li class="nav-item active"><a class="nav-link" href="#home">Home</a></li> 
+								<li class="nav-item"><a class="nav-link" href="#feature">FEATURES</a></li> 
+								<li class="nav-item"><a class="nav-link" href="#video">VIDEO</a>
+								<li class="nav-item"><a class="nav-link" href="#price">PRICING</a>
+								<li class="nav-item"><a class="nav-link" href="#screen">SCREENS</a>
+								<li class="nav-item submenu dropdown">
+									<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>
+									<ul class="dropdown-menu">
+										<li class="nav-item"><a class="nav-link" href="elements.html">Elements</a></li>
+									</ul>
+								</li> 
+								<li class="nav-item submenu dropdown">
+									<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
+									<ul class="dropdown-menu">
+										<li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
+										<li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a></li>
+									</ul>
+								</li> 
+								<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+							</ul>
+						</div>  -->
+					</div>
+            	</nav>
+            </div>
+        </header>
+        <!--================Header Menu Area =================-->
+        
+        <!--================Home Banner Area =================-->
+        <section class="home_banner_area" id="home">
+            <div class="banner_inner">
+				<div class="container">
+					<div class="row banner_content">
+						<div class="col-lg-9">
+							<h2>Silakan Unduh Android App <br />Puri Kalimaya</h2>
+							<p>Layanan yang ramah dan memuaskan akan mendampingi Anda dalam menemukan pilihan yang tepat.</p>
+							<a class="banner_btn" href="#">
+								<div class="row">
+									<img src="img/logo_play_store.png" style="width: 20px;  height: 20px; display: block; margin-top: auto; margin-bottom: auto; margin-right: 10px"/>
+									Unduh
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-3">
+							<div class="banner_map_img">
+								<img class="img-fluid" src="img/banner/right-mobile.png" alt="">
+							</div>
+							<div class="banner_map_img">
+								<img id="satu" class="img-fluid" src="img/banner/layar.png" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+            </div>
+        </section>
+        <!--================End Home Banner Area =================-->
+          
+        
+        
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/popper.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/stellar.js"></script>
+        <script src="vendors/lightbox/simpleLightbox.min.js"></script>
+        <script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
+        <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+        <script src="vendors/isotope/isotope-min.js"></script>
+        <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="js/jquery.ajaxchimp.min.js"></script>
+        <script src="vendors/counter-up/jquery.waypoints.min.js"></script>
+        <script src="vendors/counter-up/jquery.counterup.js"></script>
+        <script src="js/mail-script.js"></script>
+        <script src="vendors/popup/jquery.magnific-popup.min.js"></script>
+        <script src="js/theme.js"></script>
+
+
+        <?php
+		// $servername = "localhost";
+		// $username = "root";
+		// $password = "";
+		// $dbname = "prkalima";
+		
+		// Create connection
+		// $conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		// if ($conn->connect_error) {
+		// 	die("Connection failed: " . $conn->connect_error);
+		// }
+		
+		
+        // if (isset($_GET['phoneref'],$_GET['phonetarget'] )) {
+			// echo $_GET['phoneref'];
+			// echo $_GET['phonetarget'];
+
+		
+			// if (!checkPairAlreadyExist($_GET['phonetarget'], $conn)){
+
+				// $sql = "INSERT INTO llx_referensi (phone_ref, phone_target)
+				// VALUES ('".$_GET['phoneref']."','".$_GET['phonetarget']."')";
+	
+				// if ($conn->query($sql) === TRUE) {
+					// echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
+				// } else {
+					// echo "<script type= 'text/javascript'>alert('Error: " . $sql . "<br>" . $conn->error."');</script>";
+				// }
+	
+				// $conn->close();
+			// } else {
+				// echo "<script type= 'text/javascript'>alert('Phone target telah terdaftar');</script>";
+			// }
+	    // } 
+
+		// function checkPairAlreadyExist($phonetarget, $conn){
+
+		// 	$stmt = $conn->prepare("SELECT phone_target FROM llx_referensi WHERE phone_target = ?");
+		// 	$stmt->bind_param("s", $phonetarget);
+		// 	$stmt->execute();
+		// 	$stmt->store_result();
+		// 	return $stmt->num_rows > 0;	
+		// }
+		
+        ?>
+    </body>
+</html>
